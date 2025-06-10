91c5bbefe3bee343d0bf8e2afb58d9cb58ce3593
vitor2008s969 <vrodriguespereira11@gmail.com>
2025-06-10 15:56:30 -0300

    Update and rename sobre.html to Js/dark-mode.js

---
+++ Js/dark-mode.js
@@ -0,0 +1,7 @@
+const darkModeToggle = document.getElementById('dark-mode-toggle');
+const body = document.body;
+
+darkModeToggle.addEventListener('click', () => {
+    body.classList.toggle('dark-mode');
+    darkModeToggle.textContent = body.classList.contains('dark-mode') ? '☀️' : '🌙';
+});
